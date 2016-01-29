3c7e624744f63dbb641e4903f5ed06c33ce2eef7
Richard G. Topolewski <topolewr@cooley.edu>
2016-01-28 21:48:22 -0500

    Added code for arcade drive,
    Still need joysticks defined in Robot Build and regenerate the code.

--- src/org/usfirst/frc5053/FRC2016Stronghold/commands/ArcadeDrive.java
+++ src/org/usfirst/frc5053/FRC2016Stronghold/commands/ArcadeDrive.java
@@ -13,6 +13,7 @@ package org.usfirst.frc5053.FRC2016Stronghold.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc5053.FRC2016Stronghold.Robot;
+import org.usfirst.frc9000.FRC2016Stronghold.RobotMap;
 
 /**
  *
@@ -42,6 +43,7 @@ public class ArcadeDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        RobotMap.driveTrainRobotDrive.arcadeDrive(Robot.oi.getDriver());
     }
 
     // Make this return true when this Command no longer needs to run execute()
