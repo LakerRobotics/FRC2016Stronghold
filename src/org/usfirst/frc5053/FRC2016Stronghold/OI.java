8f55ce6188567c11db7d82c696f72695a563f897
Richard G. Topolewski <topolewr@cooley.edu>
2016-02-19 10:47:52 -0500

    This is a test commit to show how to use Git inside eclipse

--- src/org/usfirst/frc5053/FRC2016Stronghold/OI.java
+++ src/org/usfirst/frc5053/FRC2016Stronghold/OI.java
@@ -24,6 +24,7 @@ import org.usfirst.frc5053.FRC2016Stronghold.subsystems.*;
  */
 public class OI {
 	//test change
+	//test change2w
     //// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
