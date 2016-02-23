7c8ecd2eac2e0a7d057ed6d109beaa88422a52a6
Richard G. Topolewski <topolewr@cooley.edu>
2016-02-23 12:20:33 -0500

    Added first attempt at tracking location
    Added basic code track location based on wheel encoder and gyro angle
    and report it back to Smartdashboard, which should provide it to the vi
    front end.

---
+++ Arduino/i2c_slave_sensor.ino
@@ -0,0 +1,57 @@
+
+#include <Wire.h>
+#include "Arduino.h"
+
+class Ultrasonic
+{
+   public:
+      Ultrasonic(int pin);
+        void DistanceMeasure(void);
+      long microsecondsToCentimeters(void);
+      
+   private:
+      int _pin;//pin number of Arduino that is connected with SIG pin of Ultrasonic Ranger.
+        long duration;// the Pulse time received;
+};
+Ultrasonic::Ultrasonic(int pin)
+{
+   _pin = pin;
+}
+/*Begin the detection and get the pulse back signal*/
+void Ultrasonic::DistanceMeasure(void)
+{
+    pinMode(_pin, OUTPUT);
+   digitalWrite(_pin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(_pin, HIGH);
+   delayMicroseconds(5);
+   digitalWrite(_pin,LOW);
+   pinMode(_pin,INPUT);
+   duration = pulseIn(_pin,HIGH);
+}
+/*The measured distance from the range 0 to 400 Centimeters*/
+long Ultrasonic::microsecondsToCentimeters(void)
+{
+   return duration/29/2;   
+}
+
+Ultrasonic ultrasonic(7);
+
+void setup() {
+   Wire.begin(8);                // join i2c bus with address #8
+   Wire.onRequest(requestEvent); // register event
+}
+
+void loop() {
+   delay(10);
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+   long RangeInCentimeters;
+   ultrasonic.DistanceMeasure();// get the current signal time;
+   RangeInCentimeters = ultrasonic.microsecondsToCentimeters();//convert the time to centimeters
+   Wire.write(RangeInCentimeters); // respond with message of 6 bytes
+   // as expected by master
+}
